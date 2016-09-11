58cd4c46b4c07a31024e317f21c2641604bba086
Qhiliqq <philipp.esswein@me.com>
2016-09-11 11:50:30 +0200

    Create german.php

---
+++ modules/servers/easywi/lang/german.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Easy-Wi Addon
+ *
+ * @package    WHMCS
+ * @author     Ulrich Block <ulrich.block@easy-wi.com>
+ * @copyright  Copyright (c) Ulrich Block
+ * @license    http://www.gnu.org/licenses/gpl-3.0
+ * @version    1.9
+ * @link       https://www.easy-wi.com.com/
+ *
+ * The Easy-WI WHMCS addon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * The Easy-WI WHMCS addon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Easy-WI.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Diese Datei ist Teil vom The Easy-WI WHMCS Addon.
+ *
+ * Das Easy-WI WHMCS Addon ist Freie Software: Sie koennen es unter den Bedingungen
+ * der GNU General Public License, wie von der Free Software Foundation,
+ * Version 3 der Lizenz oder (nach Ihrer Wahl) jeder spaeteren
+ * veroeffentlichten Version, weiterverbreiten und/oder modifizieren.
+ *
+ * Das Easy-WI WHMCS Addon wird in der Hoffnung, dass es nuetzlich sein wird, aber
+ * OHNE JEDE GEWAEHELEISTUNG, bereitgestellt; sogar ohne die implizite
+ * Gewaehrleistung der MARKTFAEHIGKEIT oder EIGNUNG FUER EINEN BESTIMMTEN ZWECK.
+ * Siehe die GNU General Public License fuer weitere Details.
+ *
+ * Sie sollten eine Kopie der GNU General Public License zusammen mit diesem
+ * Programm erhalten haben. Wenn nicht, siehe <http://www.gnu.org/licenses/>.
+ */
+$_ADDONLANG = array(
+    "information" => "Informationen",
+    "login" => "Easy-Wi Login",
+    "rlogin" => "Passwort zurücksetzen",
+    "loginurl" => "Easy-Wi Login URL",
+    "user" => "Nutzername",
+    "pw" => "Passwort",
+    "pwr" => "Passwort wiederholen",
+);
